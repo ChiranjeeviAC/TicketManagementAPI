6e743d0e3620b087d3aba2d564c6ec1b178566ef
ChiranjeeviAC <chiranjeevi.ac.31@gmail.com>
2026-09-18 21:25:57 +0530

    Run Migration and Updated Database.

--- TicketManagementAPI/Program.cs
+++ TicketManagementAPI/Program.cs
@@ -1,30 +1,42 @@
 
+using Microsoft.EntityFrameworkCore;
+using TicketManagement.API.Data;
+
 namespace TicketManagementAPI
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+           
+
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            // Add controllers
             builder.Services.AddControllers();
-            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-            builder.Services.AddOpenApi();
+
+            // Add Entity Framework Core
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")
+                )
+            );
+
+            // Add Swagger
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
